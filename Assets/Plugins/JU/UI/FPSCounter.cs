22fd2c2696867f334487aa661dd5b2d4ca12dd6f
Maksim Tretiakov <m.tretyakov@kefirgames.com>
2024-06-09 13:44:26 +0300

    upd: assets

--- Assets/Plugins/JU/UI/FPSCounter.cs
+++ Assets/Plugins/JU/UI/FPSCounter.cs
@@ -1,18 +1,20 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 namespace JUTPS.Utilities
 {
     [AddComponentMenu("JU TPS/UI/FPS Counter")]
     public class FPSCounter : MonoBehaviour
     {
-        [SerializeField] private Text FPSText;
+        [SerializeField] private TextMeshProUGUI FPSText;
         public float RefreshRate;
         void Start()
         {
-            InvokeRepeating("UpdateFrameRateOnScreen", 0, RefreshRate);
-
+            Application.targetFrameRate = 140;
+            InvokeRepeating(nameof(UpdateFrameRateOnScreen), 0, RefreshRate);
+    
             //if that component does not have a text assigned, it will look locally for a text component.
-            if (FPSText == null && GetComponent<Text>() != null) { FPSText = GetComponent<Text>(); }
+            if (FPSText == null && GetComponent<Text>() != null) { FPSText = GetComponent<TextMeshProUGUI>(); }
         }
         public void UpdateFrameRateOnScreen()
         {
@@ -26,7 +28,7 @@ namespace JUTPS.Utilities
         /// Returns the value of the FPS(Frames per second) at the time it is called
         /// </summary>
         /// <returns></returns>
-        public static int GetFrameRate()
+        private static int GetFrameRate()
         {
             int fps = (int)(1f / Time.unscaledDeltaTime);
             return fps;
